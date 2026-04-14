be6adfcb1241ac7f4b283c725b37b4d8d481ca37
alrizaldiSHS <10rizaldiakbar@gmail.com>
2026-04-15 01:15:44 +0700

    add config deploy github

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Fallback to Babel if SWC binary fails
+  output: "export", // Export static HTML for GitHub Pages
   swcMinify: process.env.NEXT_SKIP_SWC === "1" ? false : true,
+  // Disable image optimization for static export
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
